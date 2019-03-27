fea0c442ccc712c20a4679a2a256212bcb936123
deam <deamme@users.noreply.github.com>
2019-03-27 23:34:53 +0100

    Initial page styling

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,31 @@
+import { css } from '@emotion/core'
 import { graphql } from 'gatsby'
 import React from 'react'
+import Layout from '../components/Layout'
+
+const container = css`
+  margin: 0 auto;
+  padding-top: 2rem;
+  max-width: 600px;
+`
+
+const name = css`
+  font-size: 30px;
+  font-weight: 600;
+  color: #333333;
+  text-transform: uppercase;
+`
 
 export default props => {
   const { data } = props
 
-  return <h1>{data.prismicHomepage.data.name.text}</h1>
+  return (
+    <Layout>
+      <div css={container}>
+        <h1 css={name}>{data.prismicHomepage.data.name.text}</h1>
+      </div>
+    </Layout>
+  )
 }
 
 export const pageQuery = graphql`
