ca7ddef7ad7ab45690e2c4febcae7a4bb9958236
deam <deamme@users.noreply.github.com>
2019-04-05 12:17:27 +0200

    Added description and some refactoring

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,28 +1,44 @@
 import { css } from '@emotion/core'
 import { graphql } from 'gatsby'
+import { Parser } from 'html-to-react'
 import React from 'react'
 import Layout from '../components/Layout'
 
 const container = css`
   margin: 0 auto;
-  padding-top: 2rem;
+  padding-top: 4rem;
   max-width: 600px;
+  color: #333333;
+
+  h1 {
+    font-size: 30px;
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-bottom: 2rem;
+  }
+
+  p {
+    margin-bottom: 0.25rem;
+  }
+
+  .grey {
+    color: #96999b;
+  }
 `
 
-const name = css`
-  font-size: 30px;
-  font-weight: 600;
-  color: #333333;
-  text-transform: uppercase;
-`
+const htmlToReactParser = new Parser()
 
 export default props => {
   const { data } = props
+  const content = data.prismicHomepage.data
+  const name = content.name.text
+  const description = content.description.html
 
   return (
     <Layout>
       <div css={container}>
-        <h1 css={name}>{data.prismicHomepage.data.name.text}</h1>
+        <h1>{name}</h1>
+        {htmlToReactParser.parse(description)}
       </div>
     </Layout>
   )
@@ -34,6 +50,9 @@ export const pageQuery = graphql`
       data {
         name {
           text
+        }
+        description {
+          html
         }
       }
     }
