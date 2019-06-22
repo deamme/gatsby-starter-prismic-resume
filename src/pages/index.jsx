6d654638049342bb8ea63621fe636a56a4209feb
Deam <deamme@users.noreply.github.com>
2019-06-22 19:44:26 +0200

    Added sections

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,6 +7,7 @@ import Layout from '../components/Layout'
 const container = css`
   margin: 0 auto;
   padding-top: 4rem;
+  padding-bottom: 8rem;
   max-width: 600px;
   color: #333333;
 
@@ -17,12 +18,52 @@ const container = css`
     margin-bottom: 2rem;
   }
 
+  h2 {
+    margin-top: 4rem;
+    font-size: 12px;
+    font-weight: 600;
+    color: #c9cccf;
+    text-transform: uppercase;
+    letter-spacing: 1.2px;
+  }
+
   p {
     margin-bottom: 0.25rem;
   }
 
+  a {
+    transition: box-shadow 160ms ease 0s;
+    box-shadow: rgb(238, 251, 255) 0px -9px 0px inset;
+    border-bottom: 2px solid rgb(207, 243, 255);
+    text-decoration: none;
+    color: #323336;
+    &:hover {
+      box-shadow: rgb(207, 243, 255) 0px -1.2em 0px inset;
+    }
+  }
+
+  ul {
+    margin-top: 0.5rem;
+    margin-left: 2.5rem;
+    list-style: disc;
+  }
+
+  li {
+    margin-bottom: 0.5rem;
+  }
+
   .grey {
     color: #96999b;
+  }
+
+  .headline {
+    display: block;
+    text-transform: uppercase;
+    font-weight: 600;
+    font-size: 18px;
+    letter-spacing: 1.2px;
+    padding-top: 2rem;
+    padding-bottom: 0.5rem;
   }
 `
 
@@ -33,12 +74,23 @@ export default props => {
   const content = data.prismicHomepage.data
   const name = content.name.text
   const description = content.description.html
+  const sections = content.body.map(section => {
+    const title = section.primary.title.text
+    const items = section.items.map(item => htmlToReactParser.parse(item.content.html))
+    return (
+      <div>
+        <h2>{title}</h2>
+        <div>{items}</div>
+      </div>
+    )
+  })
 
   return (
     <Layout>
       <div css={container}>
         <h1>{name}</h1>
         {htmlToReactParser.parse(description)}
+        {sections}
       </div>
     </Layout>
   )
@@ -53,6 +105,18 @@ export const pageQuery = graphql`
         }
         description {
           html
+        }
+        body {
+          primary {
+            title {
+              text
+            }
+          }
+          items {
+            content {
+              html
+            }
+          }
         }
       }
     }
