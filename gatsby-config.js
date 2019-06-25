63698a6c8da740630b65f898a154f8a1d0573a95
Deam <deamme@users.noreply.github.com>
2019-06-25 09:25:08 +0200

    Changed repo name

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-prismic',
       options: {
-        repositoryName: 'resume-test',
+        repositoryName: 'deamme',
         accessToken: process.env.API_KEY,
       },
     },
